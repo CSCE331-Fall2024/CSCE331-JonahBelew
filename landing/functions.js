477d090bba8136892a1be69b3bb23fe9cb728149
Jonah Belew <113406488+j-belew@users.noreply.github.com>
2024-09-08 23:30:46 -0500

    Style Switches

---
+++ landing/functions.js
@@ -0,0 +1,12 @@
+
+var style = true;
+
+function style1_mode()
+{
+    document.getElementById('page_style').setAttribute("href","style1.css");
+}
+
+function style2_mode()
+{
+    document.getElementById('page_style').setAttribute("href","style2.css");
+}
